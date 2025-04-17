c995a74f16bf902f2ac8d68d45da13e6eabe1dfc
f-PLT <fplt.softwaredeveloper@gmail.com>
2025-04-17 14:04:35 -0400

    new changes to test hook

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -4,6 +4,8 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
 #SBATCH --time=00:05:00
+#SBATCH --output=/home/mila/f/francis.pelletier/projects/uv-test/job_output.txt
+#SBATCH --error=/home/mila/f/francis.pelletier/projects/uv-test/job_error.txt
 
 module load python/3.10
 virtualenv $SLURM_TMPDIR/uv-test/.venv
---
+++ script.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+sbatch /home/mila/f/francis.pelletier/projects/uv-test/sbatch.sh
